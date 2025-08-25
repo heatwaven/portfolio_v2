3ed0e61848f65011f47cdcac3abf1ef904ece2cb
Marko Pavlovic <45437000+heatwaven@users.noreply.github.com>
2025-08-25 23:28:19 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // NUR wenn deine Seite unter github.io/portfolio_v2 läuft (nicht bei custom domain):
+  basePath: '/portfolio_v2',
+  assetPrefix: '/portfolio_v2',
+};
 
 export default nextConfig;
